60779283e823fd96147b7889044675eaa9207763
shoaib-jamali-dev <shoaibjamali30@gmail.com>
2026-04-17 20:46:18 +0500

    feedback project added

---
+++ feedback/script.js
@@ -0,0 +1,31 @@
+const rating = document.querySelectorAll(".rating")
+const ratingContainer = document.querySelector(".ratings-container")
+const sendBtn = document.querySelector(".btn")
+const panel = document.querySelector(".panel-container")
+
+let selectedRating = "Satisfied"
+
+ratingContainer.addEventListener("click", (e)=>{
+    if(e.target.parentNode.classList.contains("rating")){
+        removeActive();
+        e.target.parentNode.classList.add("active")
+        selectedRating = e.target.nextElementSibling.innerHTML;
+    }
+
+    
+})
+
+sendBtn.addEventListener("click", ()=>{
+    panel.innerHTML = `
+    <p class="heart">❤</p>
+    <strong> Thank You </strong>
+    <br>
+    <strong> Feedback: ${selectedRating} </strong>
+    `
+})
+
+function removeActive(){
+    for(let i = 0; i<rating.length;i++){
+        rating[i].classList.remove("active")
+    }
+}
